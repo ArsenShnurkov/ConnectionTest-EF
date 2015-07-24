af902840227d9c6fc5320b2df59473feefd7fc36
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2015-07-24 14:19:19 +0300

    Packages added, test code inserted

--- SavingTest/Program.cs
+++ SavingTest/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Data.Entity;
 
-namespace SavingTest
+public class Person
 {
-	class MainClass
+	public int Id {
+		get;
+		set;
+	}
+
+	public string FirstName {
+		get;
+		set;
+	}
+
+	public string LastName {
+		get;
+		set;
+	}
+}
+public class SimpleContext : DbContext
+{
+	public SimpleContext () : base("name = ConnStr")
 	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
+	}
+	public DbSet<Person> People { 
+		get; 
+		set;
+	}
+}
+
+class MainClass
+{
+	public static void Main (string[] args)
+	{
+		Console.WriteLine ("starting...");
+		using (var context = new SimpleContext ()) {
+			var person = new Person { 
+				FirstName = "Joe", 
+				LastName = "Bloggs" 
+			};
+
+			context.People.Add (person);
+			context.SaveChanges ();
 		}
 	}
 }
+
+/*
+A null was returned after calling the 'get_ProviderFactory' method on a store provider instance of type 'System.Data.Hsql.SharpHsqlConnection'.
+The store provider might not be functioning correctly.
+*/
