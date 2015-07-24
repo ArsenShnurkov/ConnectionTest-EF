33b01918c50e68e5b4c0ab509c2ebfe34d5070ad
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2015-07-24 18:44:16 +0300

    Some changes for debugging I hit an error in transaction implementation, don't know what to do next

--- SavingTest/Program.cs
+++ SavingTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.Entity;
+using System.Reflection;
 
 public class Person
 {
@@ -33,7 +34,10 @@ class MainClass
 {
 	public static void Main (string[] args)
 	{
-		Console.WriteLine ("starting...");
+		Assembly a = Assembly.LoadFrom ("SharpHsql.dll");
+		Console.WriteLine (a.CodeBase);
+		Assembly al = Assembly.LoadFrom ("SharpHsql.Linq.dll");
+		Console.WriteLine (al.CodeBase);
 		using (var context = new SimpleContext ()) {
 			var person = new Person { 
 				FirstName = "Joe", 
@@ -49,4 +53,40 @@ class MainClass
 /*
 A null was returned after calling the 'get_ProviderFactory' method on a store provider instance of type 'System.Data.Hsql.SharpHsqlConnection'.
 The store provider might not be functioning correctly.
+*/
+/*
+Failed to find or load the registered .Net Framework Data Provider.
+DbProviderFactory System.Data.Common.DbProviderFactories:GetFactory (DataRow)
+*/
+/*
+No Entity Framework provider found for the ADO.NET provider with invariant name 'System.Data.SharpHSQL'.
+Make sure the provider is registered in the 'entityFramework' section of the application config file.
+See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.
+*/
+/*
+The Entity Framework provider type 'MyProvider.MyProviderServices, MyAssembly' registered in the application config file 
+for the ADO.NET provider with invariant name 'My.Invariant.Name' could not be loaded.
+Make sure that the assembly-qualified name is used and that the assembly is available to the running application.
+*/
+/*
+Entity Framework providers must declare a static property or field named 'Instance' that returns the singleton instance of the provider.
+*/
+/*
+Not all resources was transferred
+		System.Data.Hsql.Linq.SharpHsqlProviderManifest.GetXmlResource (resourceName="System.Data.SharpHsql.Linq.Resources.SharpHsqlProviderServices.ProviderManifest.xml") 
+		in /var/calculate/remote/distfiles/egit-src/SharpHSQL.git/src/SharpHsql.Linq/SharpHsqlProviderManifest.cs:305
+*/
+/*
+DatabaseExists is not supported by the provider.
+		http://stackoverflow.com/questions/6101812/use-entity-framework-4-1-with-sqlite
+		you have a problem because your provider doesn't support all necessary functionality used by EF code first. 
+		http://w3facility.org/question/databaseexists-is-not-supported-by-the-provider-efwrappertoolkit/
+		more detailed description - 
+		DbProviderServicesBase in the DbDatabaseExists method
+
+		NpgSql does like this:
+		https://github.com/npgsql/npgsql/pull/91/files#diff-8eba631855901aefb64b3ab6cafe4489R113
+*/
+/*
+Unexpected connection state. When using a wrapping provider ensure that the StateChange event is implemented on the wrapped DbConnection.
 */
